88ae69500dc8cb04114e0f525b5f79f5fa1adb1d
lee <levileele@gmail.com>
2013-10-30 16:43:53 +0200

    added static controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '6149acdffc3e046b7006fa49aee04d5e7f5132d32743ba6d0a7546e1505a329baba8b8890d82bfaa13f76b7a743d07a92bf22d0a7e5ba42249c7030502d5d7ae'
